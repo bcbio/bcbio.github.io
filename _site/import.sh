06b7dfd373f1faeedb4ff29ec749670385266116
chapmanb <chapmanb@50mail.com>
2015-09-14 19:59:06 -0400

    Avoid transition animations, remove built _site directory from jekyll

--- _site/import.sh
+++
@@ -1,5 +0,0 @@
-ruby -rubygems -e 'require "jekyll-import";
-    JekyllImport::Importers::WordpressDotCom.run({
-       "no_fetch_images" => true,
-       "source" => "bluecollarbioinformatics.wordpress.2015-09-13.xml",
-     })'
